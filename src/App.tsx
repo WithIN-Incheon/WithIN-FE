0bd9484c892cddf4941cb29173e14d9ee7dbd7d0
kim yerin <wbflqldks90@inha.deu>
2025-09-04 23:41:28 +0900

    feat: 가입 완료시 홈으로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Home from './pages/Home/Home'
+import Home from './pages/Home/HomePage'
 import SignupPage from './pages/SignupPage/SignupPage'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
