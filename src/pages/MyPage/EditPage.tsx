8d1e66fe62ae650b53db392ac7eef2f5cb40b16d
kim yerin <wbflqldks90@inha.deu>
2025-09-29 16:38:01 +0900

    feat: mypage 안 datpage,editpage 틀만 생성

---
+++ src/pages/MyPage/EditPage.tsx
@@ -0,0 +1,15 @@
+// 마이페이지-정보 수정
+import Header from "../../components/Header/Header";
+import BottomBar from "../../components/BottomBar/BottomBar";
+
+
+export default function EditPage() {
+  return (
+    <div>
+      <Header title="내 정보 수정" />
+      {/* 아래에 페이지 본문 */}
+      <BottomBar/>
+      
+    </div>
+  );
+}
