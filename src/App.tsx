b0b45f5716fb9cdf6599779d7fcc0ce86387887d
kim yerin <wbflqldks90@inha.deu>
2025-09-29 16:34:27 +0900

    feat: 라우팅 경로 추가 (/location, /favorite 등)

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,27 @@ import './App.css'
 import HomePage from './pages/HomePage/HomePage'
 import SignupPage from './pages/SignupPage/SignupPage'
 import CasePage from './pages/CasePage/CasePage'
+import CaseDetailPage from "./pages/CasePage/CaseDetailPage"
 import GuidePage from './pages/Comp-GuidePage/Comp-GuidePage'
-import HospitalPage from './pages/HospitalPage/HospitalPage'
-import BookmarkPage from './pages/BookmarkPage/BookmarkPage'
+import FavoritePage from './pages/FavoritePage/FavoritePage'
 import MyPage from './pages/MyPage/MyPage'
+import LocationPage from "./pages/LocationPage/LocationPage";
+import BookmarkPage from "./pages/LocationPage/BookmarkPage/BookmarkPage";
+import SearchPage from "./pages/LocationPage/SearchPage/SearchPage";
+
+
 import InfoPage from "./pages/InfoPage/InfoPage";
+import InfoCompPage from "./pages/InfoPage/InfoCompPage/InfoCompPage";
+import InfoProcessPage from "./pages/InfoPage/InfoProcessPage/InfoProcessPage";
+import InfoSalaryPage  from "./pages/InfoPage/InfoSalaryPage/InfoSalaryPage";
+import InfoDictionPage  from "./pages/InfoPage/InfoDictionPage/InfoDictionPage";
+import InfoTipPage from "./pages/InfoPage/InfoTipPage/InfoTipPage";
+import InfoCenterPage from "./pages/InfoPage/InfoCenterPage/InfoCenterPage";
+
+
 import HelperPage from "./pages/HelpPage/HelpPage";
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
@@ -18,13 +31,27 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/home" element={<HomePage />} />
-        <Route path="/case" element={<CasePage />} />
+        <Route path="/cases" element={<CasePage />} />
+        <Route path="/cases/:id" element={<CaseDetailPage />} />
+
         <Route path="/guide" element={<GuidePage/>} />
-        <Route path="/hospital" element={<HospitalPage/>} />
-        <Route path="/bookmark" element={<BookmarkPage/>} />
+        <Route path="/location" element={<LocationPage/>} />
+        <Route path="/favorite" element={<FavoritePage/>} />
         <Route path="/mypage" element={<MyPage/>} />
-        <Route path="/info" element={<InfoPage />} />
         <Route path="/helper" element={<HelperPage />} />
+        <Route path="/location " element={<LocationPage  />} />
+        <Route path="/location/bookmark" element={<BookmarkPage />} />
+        <Route path="/location/search" element={<SearchPage />} />
+
+        <Route path="/info" element={<InfoPage />} />
+        <Route path="/info/compensation" element={<InfoCompPage />} />
+        <Route path="/info/process" element={<InfoProcessPage />} />
+        <Route path="/info/salary" element={<InfoSalaryPage />} />
+        <Route path="/info/dictionary" element={<InfoDictionPage/>} />
+        <Route path="/info/tip" element={<InfoTipPage/>} />
+        <Route path="/info/center" element={<InfoCenterPage />} />
+        
+
       </Routes>
     </Router>
   );
