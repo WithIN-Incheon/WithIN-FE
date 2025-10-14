08745ff37a0b59c8e8909e09ce569f31c35ee302
kim yerin <wbflqldks90@inha.deu>
2025-10-14 11:33:43 +0900

    delete:불필요한 즐겨찾기 등 기능 삭제

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -28,11 +28,6 @@ export default function Header({
   showMenu = false,
   onMenuClick,
 
-  showBookmark = false,
-  bookmarkTo = "/location/bookmark",
-  bookmarkIconSrc = "/bookmark-icon.svg",
-  onBookmarkClick,
-
   showSearch = false,
   onSearchClick,
 }: HeaderProps) {
@@ -40,7 +35,6 @@ export default function Header({
 
   const handleBack = () => (onBack ? onBack() : navigate(-1));
   const handleMenu = () => onMenuClick?.();
-  const handleBookmark = () => (onBookmarkClick ? onBookmarkClick() : navigate(bookmarkTo));
   const handleSearch = () => onSearchClick?.();
 
   return (
@@ -71,17 +65,7 @@ export default function Header({
         />
       )}
 
-      {showBookmark && (
-        <img
-          src={bookmarkIconSrc}
-          alt="즐겨찾기"
-          className="header_bookmark"
-          onClick={handleBookmark}
-          role="button"
-          tabIndex={0}
-          onKeyDown={(e) => (e.key === "Enter" || e.key === " ") && handleBookmark()}
-        />
-      )}
+
 
       {showSearch && (
         <img
